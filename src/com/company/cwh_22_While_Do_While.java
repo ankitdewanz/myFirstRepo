72c386fa6fe7614683d6c0cffd5e2d74d06122ac
Ankit <i.ankitdewan@gmail.com>
2022-12-25 14:37:46 -0500

    Programs for video from 16 to 21

--- src/com/company/cwh_22_While_Do_While.java
+++ src/com/company/cwh_22_While_Do_While.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class cwh_22_While_Do_While {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <=5){
+        System.out.println(i);
+        i++;
+        }
+
+        do{
+        System.out.println(i);
+        i--;
+        }while (i>=0);
+}
 }
